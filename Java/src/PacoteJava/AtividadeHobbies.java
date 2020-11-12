a08755282dba94c879c879ec059c9d6d6f50f0f4
Cinthia <cinthia.tengan@gmail.com>
2020-11-12 15:23:33 -0300

    Transferência de atividades para o Github

---
+++ Java/src/PacoteJava/AtividadeHobbies.java
@@ -0,0 +1,12 @@
+package PacoteJava;
+
+public class AtividadeHobbies {
+	public static void main(String[] args) {
+		System.out.println("Olá, meu nome é Cinthia Tengan e meus hobbies são:");
+		System.out.println("Desenhar, jogar jogos no computador, ver animes e pescar.");
+		System.out.println("Não gosto de:");
+		System.out.println("Preconceito (racismo, sexismo, homofobia), fanatismos (de qualquer tipo) e pessoas baixo astral.");
+		
+	}
+
+}
---
+++ Java/src/PacoteJava/MentalidadesHabilidades.java
@@ -0,0 +1,11 @@
+package PacoteJava;
+
+public class MentalidadesHabilidades {
+	public static void main(String[] args) {
+		System.out.println("Para chegar até aqui, acredito que tenha utilizado muito a mentalidade de crescimento e persistência.");
+		System.out.println("A Mentalidade de crescimento para me adaptar as mudanças e nunca ficar estagnada, sempre buscando uma");
+		System.out.println("solução sobre o que estudar e me adaptar ao mercado de trabalho. A persistência se deve ao fato de que");
+		System.out.println("mesmo com vários 'nãos', eu nunca desisti de tentar e de dar sempre o melhor de mim em tudo.");
+	}
+
+}
